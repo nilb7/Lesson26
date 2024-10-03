93c60f693bfa17947509a47d9a17a24b6c65e7fd
nilb7 <tieenn298@gmail.com>
2024-10-03 15:58:08 +0200

    made some variables

---
+++ js/main.js
@@ -0,0 +1,15 @@
+console.log("javascript")
+
+var x = 10
+var teksti = "Edeni"
+var truth = true
+
+var a = 10+20
+var b = 20-10
+var c = 10/2
+var d = 30 % 5
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
