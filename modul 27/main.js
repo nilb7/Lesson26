cebcb3d74a4d9f211fb492d0fcd2b50ac50d3244
nilb7 <tieenn298@gmail.com>
2024-10-08 16:00:48 +0200

    template literals

---
+++ modul 27/main.js
@@ -0,0 +1,18 @@
+var a = 10;
+var b = 12;
+var c = 18;
+var d = 15;
+
+console.log(a < b && c > d)
+console.log(a > b && c < d)
+console.log(a < b || c > d)
+console.log (!(a < b))
+
+var school =('Digial School')
+
+// template literals
+
+console.log("My school name is "   +(school))
+
+
+
