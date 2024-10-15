795331ba0ea9dd40a84781d9ccefff1db5548afb
nilb7 <tieenn298@gmail.com>
2024-10-15 15:58:44 +0200

    ds

---
+++ module28/main.js
@@ -0,0 +1,25 @@
+var button1 = document.getElementById("btn1")
+var button2 = document.getElementById("btn2")
+var button3 = document.getElementById("btn3")
+var button4 = document.getElementById("btn4")
+
+button1.onclick = function(){
+    ds_text.style.color='blue';
+    ds_text.style.textAlign='center';
+    ds_text.style.fontSize='50px';
+
+}
+
+button2.onmouseover = function(){
+    alert('button 2 was hovered')
+}
+
+button3.onmouseleave = function(){
+    alert('button 3 was left')
+}
+
+button4.onwheel = function(){
+    alert('button 4 was wheeled')
+}
+
+
