c407143e8363bd802842d2e8c171b270873ed9ee
nilb7 <tieenn298@gmail.com>
2024-10-15 16:17:21 +0200

    module28

--- module28/main.js
+++ module28/main.js
@@ -3,11 +3,13 @@ var button2 = document.getElementById("btn2")
 var button3 = document.getElementById("btn3")
 var button4 = document.getElementById("btn4")
 var ds_text = document.getElementById("txt1")
+var js_text = document.getElementById("txt2")
 
 button1.onclick = function(){
     ds_text.style.color='blue';
     ds_text.style.textAlign='center';
     ds_text.style.fontSize='50px';
+    js_text.setAttribute('class', 'test')
 
 }
 
