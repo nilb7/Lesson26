25e02ac167d15188ac7ec32185ae5e5ece4d8152
nilb7 <tieenn298@gmail.com>
2024-10-17 16:10:31 +0200

    DS

--- module29/main.js
+++ module29/main.js
@@ -1,3 +1,18 @@
 var subjects = ["Math","Language","Science","Physics","PE","Art","Biology","Geography","Deutch","English"];
 
 console.log(subjects[5]);
+
+subjects.push("Albanian");
+console.log(subjects);
+
+subjects.pop();
+console.log(subjects);
+
+subjects.unshift('23');
+console.log(subjects);
+
+subjects.shift();
+console.log(subjects);
+
+
+
