d88b75fb1ddc0040b1e1a918c8cb31e9e47487b9
nilb7 <tieenn298@gmail.com>
2024-10-31 16:14:32 +0100

    aa

--- module32/main1.js
+++ module32/main1.js
@@ -10,4 +10,7 @@ var regex1= /[abc]/g;
 console.log(text1.match(regex1))
 
 var regex2=/[0-9]/g;
-console.log(text.match(regex2))
+console.log(text1.match(regex2))
+
+var regex3=/(top|best|school)/g;
+console.log(text1.match(regex3))
