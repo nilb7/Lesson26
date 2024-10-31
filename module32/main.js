e2abf12fe9082a5820e741d1ef1bcf7ff996a6c0
nilb7 <tieenn298@gmail.com>
2024-10-31 15:31:44 +0100

    gf

--- module32/main.js
+++ module32/main.js
@@ -5,3 +5,12 @@ var text = document.getElementById("texti_id")
 button.onclick = function(){
     text.innerHTML = input.value;
 }
+
+var input1 = document.getElementById("input1")
+var input2 = document.getElementById("input2")
+var answer = document.querySelector("#answer");
+var btn_shuma = document.querySelector("#bt_shuma");
+
+btn_shuma.addEventListener("click", function(){
+    answer.innerHTML = input1.value+input2.value;
+})
