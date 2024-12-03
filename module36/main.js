bf12b2ab1bc147d886f1ec200cd0c34782a2831b
nilb7 <tieenn298@gmail.com>
2024-12-03 16:16:34 +0100

    hhsdfd

--- module36/main.js
+++ module36/main.js
@@ -1,5 +1,21 @@
-var and = document.getElementById('testElement');
 
-and.style.color = "red";
 
-$('#testElement').css("color", "red")
+
+$("li").click(function(){
+    alert("This was clicked");
+});
+    
+
+$("#btn1").click(function(){
+    $("#h1").text("Anik dhe bled")
+    $("#h1").append(" dhe EDEN")
+})
+
+$("#btn2").on("click",function(){
+    console.log('This is  heading')
+})
+
+$("#div1").click(function(){
+    $(this).addClass("clicked");
+}).find("span").attr("title", "this is a changed title");
+
