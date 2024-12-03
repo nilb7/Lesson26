12a811dc52805b522cfb5f149200116fbf63a183
nilb7 <tieenn298@gmail.com>
2024-12-03 15:36:05 +0100

    ss

---
+++ main.js
@@ -0,0 +1,10 @@
+// var anik = document.getElementById("p")
+var button = document.getElementById("button")
+
+
+// button.onclick = function(){
+//         anik.style.color='blue';
+// }
+
+var anik = document.getElementById("p")
+button.addEventListener("click", function(){anik.style.color="blue"});
